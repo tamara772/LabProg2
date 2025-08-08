6459a1696c6f832ee73ac33191a15c12fb114dd3
tamara772 <tpedron@gmail.com>
2025-08-08 16:57:09 -0300

    Update etapa1.c

--- etapa1.c
+++ etapa1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main (void)
 {
-    printf("Nome: Isabel tamara Pedron \n Data de nascimento: 05/02/1963 \n Curso: Ciência da Computação\n");
+printf("Nome: Isabel tamara Pedron \n Data de nascimento: 05/02/1963 \n Curso: Ciência da Computação\n");
    
 
 printf("\n Quanta tristeza\n Há nesta vida \n Só incerteza\n Só despedida \n Amar é triste \n O que é que existe?\n O amor\n");
@@ -10,7 +10,7 @@ int i;
     for (i = 1; i <= 10; i++)
     {
     valor=valor+7;
-    printf("7x{i}= %d \n",valor);
+      printf("7 x %d = %d\n", i, valor);
    }
 
     
